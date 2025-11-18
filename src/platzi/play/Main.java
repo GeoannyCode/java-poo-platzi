afac4d7489c3306ad26a4f6cf86763340c5d515e
Diego Bracero <diegobracero@hotmail.com>
2025-11-18 12:34:22 -0500

    Define VERSION as constant

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -9,8 +9,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
+    public static final String VERSION = "1.0.0";
+
     public static void main(String[] args) {
-        System.out.println(" **** PLATZI PLAY **** ");
+        System.out.println(" **** PLATZI PLAY **** v" + VERSION);
 
         String name = ScannerUtils.inputText("Content name");
         String genre = ScannerUtils.inputText("Content genre");
--- src/platzi/play/util/ScannerUtils.java
+++ src/platzi/play/util/ScannerUtils.java
@@ -3,26 +3,26 @@ package platzi.play.util;
 import java.util.Scanner;
 
 public class ScannerUtils {
-    public static Scanner scanner = new Scanner(System.in);
+    public static final Scanner SCANNER = new Scanner(System.in);
 
     public static String inputText(String message){
         System.out.println(message + ": ");
-        return scanner.nextLine();
+        return SCANNER.nextLine();
     }
 
     public static int inputNumber(String message){
         System.out.println(message + ": ");
 
-        int data = scanner.nextInt();
-        scanner.nextLine();
+        int data = SCANNER.nextInt();
+        SCANNER.nextLine();
         return data;
     }
 
     public static double inputDecimalValue(String message){
         System.out.println(message + ": ");
 
-        double data = scanner.nextDouble();
-        scanner.nextLine();
+        double data = SCANNER.nextDouble();
+        SCANNER.nextLine();
         return data;
     }
 
